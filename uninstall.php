07f2c59ba1a1fc3e18ff315a988496ac5b06bfb1
Caity Ross <internet@pandammonium.org>
2022-08-28 18:57:49 +0100

    Refactors plugin: moves Blipper_Widget class to separate file.
    Adds checks for functions and classes already being defined.
    
    Makes a start on improving documentation (for phpdoc).
    
    Adds file and line number to selected error messages.

--- uninstall.php
+++ uninstall.php
@@ -1,75 +1,77 @@
 <?php
 
 /**
-  * Blipper Widget — uninstallation
-  * @since 0.0.3
+  * Uninstalls Blipper Widget.
+  * Uninstalls Blipper Widget and removes the options stored in the database.
+  * Some of this code is unashamedly swiped from the uninstall.php file
+  * of the WP-Spamshield plugin.
   *
-  * This files uninstalls Blipper Widget and removes the options stored in the
-  * database.
-  *
-  * Some of this code is unashamedly swiped from the uninstall.php file of the
-  * WP-Spamshield plugin.
+  * @author   pandammonium
+  * @since    0.0.3
+  * @license  GPLv2 or later
   */
 
+// If this file is called directly, abort:
+defined( 'ABSPATH' ) or die();
+defined( 'WPINC' ) or die();
 // If uninstall not called from WordPress, exit:
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
   exit();
 }
 
+if (!function_exists( 'blipper_widget_uninstall' )) {
 /**
   * @since 0.0.3
   */
-function blipper_widget_uninstall() {
+  function blipper_widget_uninstall() {
 
-  if ( current_user_can( 'edit_plugins' ) ) {
+    if ( current_user_can( 'edit_plugins' ) ) {
 
-    // Delete options in database
-    $option_name = 'blipper-widget-settings-oauth';
-    delete_option( $option_name );
-    // For site options in multi-site:
-    delete_site_option( $option_name );
+      // Delete options in database
+      $option_name = 'blipper-widget-settings-oauth';
+      delete_option( $option_name );
+      // For site options in multi-site:
+      delete_site_option( $option_name );
 
-    // Unregister the widget
-    unregister_widget( 'Blipper_Widget' );
+      // Unregister the widget
+      unregister_widget( 'Blipper_Widget' );
 
-    // Clean up widget options
-    $sidebar_widgets = get_option( 'sidebars_widgets' );
-    foreach ( $sidebar_widgets as $key => $value ) {
-      if ( is_array( $value ) ) {
-        foreach ( $value as $inner_key => $inner_value ) {
-          if ( false !== strpos( $inner_value, 'blipper_widget' ) ) {
-            // Don't want to mess with any widget that isn't the Blipper Widget.
-            unset( $sidebar_widgets[$key][$value] );
+      // Clean up widget options
+      $sidebar_widgets = get_option( 'sidebars_widgets' );
+      foreach ( $sidebar_widgets as $key => $value ) {
+        if ( is_array( $value ) ) {
+          foreach ( $value as $inner_key => $inner_value ) {
+            if ( false !== strpos( $inner_value, 'blipper_widget' ) ) {
+              // Don't want to mess with any widget that isn't the Blipper Widget.
+              unset( $sidebar_widgets[$key][$value] );
+            }
           }
+          // Tidy up the array
+          $sidebar_widgets[$key] = array_values( $sidebar_widgets[$key] );
         }
-        // Tidy up the array
-        $sidebar_widgets[$key] = array_values( $sidebar_widgets[$key] );
       }
-    }
-    update_option( 'sidebars_widgets', $sidebar_widgets );
+      update_option( 'sidebars_widgets', $sidebar_widgets );
 
-    // Delete orphaned options
-    $all_options = wp_load_alloptions();
-    foreach ( $all_options as $key => $value ) {
-      if ( false !== strpos( $key, 'blipper_widget'  ) ) {
-        delete_option( $key );
-        // For options in multi-site:
-        delete_site_option( $key );
-      }
-      if ( false !== strpos( $key, 'blipper-widget'  ) ) {
-        delete_option( $key );
-        // For options in multi-site:
-        delete_site_option( $key );
+      // Delete orphaned options
+      $all_options = wp_load_alloptions();
+      foreach ( $all_options as $key => $value ) {
+        if ( false !== strpos( $key, 'blipper_widget'  ) ) {
+          delete_option( $key );
+          // For options in multi-site:
+          delete_site_option( $key );
+        }
+        if ( false !== strpos( $key, 'blipper-widget'  ) ) {
+          delete_option( $key );
+          // For options in multi-site:
+          delete_site_option( $key );
+        }
       }
     }
+
   }
-
 }
-
 blipper_widget_uninstall();
 
-/**
-  * Sorry to see you go.  Bye bye!
-  */
+/** Sorry to see you go.  Bye bye! */
 
 
