a58fe29a77f53202c4d16f8700630f8008e5554c
Caity Ross <13066721+pandammonium@users.noreply.github.com>
2025-02-21 02:03:37 +0000

    Uncomments widget deletion code.
    Adds debug code.
    
    Contributes to BW-53.

--- classes/class-widget.php
+++ classes/class-widget.php
@@ -164,12 +164,12 @@ if (!class_exists('Blipper_Widget')) {
         callback: [ self::class, 'bw_save_old_shortcode_attributes' ]
       );
 
-      // add_action(
-      //   hook_name: 'delete_widget',
-      //   callback: [ self::class, 'bw_on_delete_widget' ],
-      //   priority: 10,
-      //   accepted_args: 3
-      // );
+      add_action(
+        hook_name: 'delete_widget',
+        callback: [ self::class, 'bw_on_delete_widget' ],
+        priority: 10,
+        accepted_args: 3
+      );
 
       add_shortcode(
         tag: 'blipper_widget',
@@ -2681,20 +2681,29 @@ if (!class_exists('Blipper_Widget')) {
 
     // --- Action hooks ------------------------------------------------------- //
 
-    // /**
-    //  * Removes data that doesn't need storing if an instance of the widget is
-    //  * removed.
-    //  *
-    //  * @author pandammonium
-    //  * @since 1.2.6
-    //  */
-    // public static function bw_on_delete_widget( $instance, $widget, $sidebar_id ) {
-    //   // bw_log( 'method', __METHOD__ . '()' );
-    //   // bw_log( 'arguments', func_get_args() );
+    /**
+     * Removes data that doesn't need storing if an instance of the widget is
+     * removed.
+     *
+     * @author pandammonium
+     * @since 1.2.6
+     *
+     * @param $instance
+     * @param $widget
+     * @param sidebar_id
+     */
+    public static function bw_on_delete_widget( $instance, $widget, $sidebar_id ) {
+      bw_log( 'method', __METHOD__ . '()' );
+      bw_log( 'arguments', func_get_args() );
 
-    //   // Log or perform actions when a widget is removed
-    //   error_log( 'Widget removed: ' . $widget->id_base . ' from sidebar: ' . $sidebar_id );
-    // }
+      $args = func_get_args();
+      foreach( $args as $key => $value ) {
+        error_log( gettype( $key ) . ' ' . gettype( $value ) );
+      }
+
+      // Log or perform actions when a widget is removed
+      error_log( 'Widget ' . $widget->id_base . ' removed from sidebar ' . $sidebar_id );
+    }
 
     /**
      * Checks the Blipfoto OAuth settings have been set, otherwise displays a
